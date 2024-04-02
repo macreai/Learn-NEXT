96c0cf5d7d621d340a305ba29490010d29db53e3
macreai <ardaardiansyah24@gmail.com>
2024-04-02 23:49:13 +0700

    Add Index Page

---
+++ sanity/schemaTypes/index.ts
@@ -0,0 +1,5 @@
+import category from "./category";
+import hero_images from "./hero_images";
+import product from "./product";
+
+export const schemaTypes = [product, category, hero_images]
---
+++ sanity/schemaTypes/product.ts
@@ -0,0 +1,46 @@
+export default {
+    name: 'product',
+    type: 'document',
+    title: 'Product',
+    fields: [
+        {
+            name: 'name',
+            type: 'string',
+            title: 'Name of Product'
+        },
+        {
+            name: 'images',
+            type: 'array',
+            title: 'Product Images',
+            of: [{type: 'image'}]
+        },
+        {
+            name: 'desctiption',
+            type: 'text',
+            title: 'Description of Product'
+        },
+        {
+            name: 'slug',
+            type: 'slug',
+            title: 'Product Slug',
+            options: {
+                source: 'name'
+            }
+        },
+        {
+            name: 'price',
+            type: 'number',
+            title:  'Price of Product'
+        },
+        {
+            name: 'category',
+            type: 'reference',
+            to: [
+                {
+                    type: 'category'
+                }
+            ],
+            title: 'Category of Product'
+        }
+    ]
+}
