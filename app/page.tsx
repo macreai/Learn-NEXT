e50bfa1eae5cea505d4b0993f941a6f7d01ddd1e
macreai <ardaardiansyah24@gmail.com>
2024-04-03 16:49:31 +0700

    fix page error

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,9 @@ import Newest from "./components/newest";
 export default function Home() {
   return (
     <div className="bg-white pb-6 sm:pb-8 lg:pb-12">
+      {/* @ts-expect-error Server Component */}
       <Hero />
+      {/* @ts-expect-error Server Component */}
       <Newest />
     </div>
     );
